29646d1b6354a62ea451b908a3f93ac45ae968b3
HUGO <kerry1122hu@gmail.com>
2026-03-31 19:14:47 +0800

    collapse member content groups and bust pwa cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "mingme-v1.0.0";
+const CACHE_VERSION = "mingme-v1.0.1";
 const STATIC_CACHE = `static-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `runtime-${CACHE_VERSION}`;
 const APP_SHELL = [
