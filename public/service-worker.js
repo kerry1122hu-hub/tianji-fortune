f0859a5c431f70cfb4cf850c82df51d13b273cfd
HUGO <kerry1122hu@gmail.com>
2026-04-02 10:18:48 +0800

    bust pwa cache after white screen

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "mingme-v1.0.1";
+const CACHE_VERSION = "mingme-v1.0.2";
 const STATIC_CACHE = `static-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `runtime-${CACHE_VERSION}`;
 const APP_SHELL = [
